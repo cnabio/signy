565670b72cca9ef418a1d541ccc2feaa500d0755
Trishank K Kuppusamy <trishank.kuppusamy@datadoghq.com>
2020-02-10 17:44:59 -0500

    reset script
    Signed-off-by: Trishank K Kuppusamy <trishank.kuppusamy@datadoghq.com>

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,7 +5,4 @@ NOTARY=$GOPATH/src/github.com/theupdateframework/notary
 (cd $NOTARY; docker-compose down)
 
 docker stop registry
-docker rm registry
-rm -rf ~/.signy
-rm -rf ~/.docker/trust/tuf/localhost:5000
 docker ps
