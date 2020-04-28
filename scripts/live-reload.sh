49fdff6c75fe95c7fbdd9218625d535b208a0a9f
Trishank Karthik Kuppusamy <trishank.kuppusamy@datadoghq.com>
2020-04-28 17:42:50 -0400

    tag with commit sha instead of tag
    Signed-off-by: Trishank Karthik Kuppusamy <trishank.kuppusamy@datadoghq.com>

--- scripts/live-reload.sh
+++ scripts/live-reload.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+TAG=$(git rev-parse --verify HEAD)
+
+build () {
+    echo "Building..."
+    # Build an image containing python-in-toto to verify bundles/images with.
+    docker build --rm -t cnabio/signy-in-toto-verifier:$TAG -f Dockerfiles/in-toto-container.Dockerfile Dockerfiles/
+    make install
+    echo "...done."
+    echo
+}
+
 echo "Installing fswatch..."
 brew install fswatch
 echo
 
-echo "Building..."
-make install
-echo "...done."
-echo
+build
 
 # https://emcrisostomo.github.io/fswatch/doc/1.14.0/fswatch.html/Tutorial-Introduction-to-fswatch.html#Detecting-File-System-Changes
 # NOTE: We exclude bin/* to avoid infinite loop.
 # TODO: Exclude *.sh, *.md, and other non-source files.
 # FIXME: Sometimes fswatch fires a few times in a row. It is what it is.
-fswatch -o . -e "bin/*" | (while read; echo "Building..."; do make install; date; echo "...done."; echo; done)
+fswatch -o . -e "bin/*" | (while read; do build; done)
