79f1a5bba2c1ec3b0235c9ccf1e8a9cadd98718e
Scott Buckel <buckel_scott@bah.com>
2021-04-28 11:32:46 -0400

    Copy current directory to temp directory for intoto verification
    Signed-off-by: Scott Buckel <buckel_scott@bah.com>

--- pkg/intoto/verify.go
+++ pkg/intoto/verify.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/theupdateframework/notary/client"
@@ -53,6 +54,17 @@ func getVerificationDir(target *client.TargetWithRole, bundle []byte) (string, e
 		return "", err
 	}
 
+	/*
+		TODO: Figure out a better way to do this.
+		For example, if we have a tar.gz file that was not pushed to notary that needs verified, we need to figure out a way to run in-toto verification on it.
+		For a lot of in-toto demo example, it uses a .tar.gz example. In the future since we're using docker images, it should pull all data for verification from the image itself.
+	*/
+	log.Infof("Copy all files to the temp directory %v for verification", verificationDir)
+	err = copy("/Users/scottbuckel/Work/signy/signy/bin", verificationDir)
+	if err != nil {
+		return "", err
+	}
+
 	log.Infof("Writing in-toto metadata files into %v", verificationDir)
 	err = WriteMetadataFiles(m, verificationDir)
 	if err != nil {
@@ -66,4 +78,23 @@ func getVerificationDir(target *client.TargetWithRole, bundle []byte) (string, e
 	}
 
 	return verificationDir, nil
+}
+
+func copy(source, destination string) error {
+	var err error = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		var relPath string = strings.Replace(path, source, "", 1)
+		if relPath == "" {
+			return nil
+		}
+		if info.IsDir() {
+			return os.Mkdir(filepath.Join(destination, relPath), 0755)
+		} else {
+			var data, err1 = ioutil.ReadFile(filepath.Join(source, relPath))
+			if err1 != nil {
+				return err1
+			}
+			return ioutil.WriteFile(filepath.Join(destination, relPath), data, 0777)
+		}
+	})
+	return err
 }
