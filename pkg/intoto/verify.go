fff02f4465511a30d30bac51b3fd21a601a5a0eb
Scott Buckel <buckel_scott@bah.com>
2021-05-05 11:00:05 -0400

    Fixing lint issue
    Signed-off-by: Scott Buckel <buckel_scott@bah.com>

--- pkg/intoto/verify.go
+++ pkg/intoto/verify.go
@@ -89,13 +89,13 @@ func copy(source, destination string) error {
 		}
 		if info.IsDir() {
 			return os.Mkdir(filepath.Join(destination, relPath), 0755)
-		} else {
-			var data, err1 = ioutil.ReadFile(filepath.Join(source, relPath))
-			if err1 != nil {
-				return err1
-			}
-			return ioutil.WriteFile(filepath.Join(destination, relPath), data, 0777)
 		}
+		var data, err1 = ioutil.ReadFile(filepath.Join(source, relPath))
+		if err1 != nil {
+			return err1
+		}
+		return ioutil.WriteFile(filepath.Join(destination, relPath), data, 0777)
+
 	})
 	return err
 }
